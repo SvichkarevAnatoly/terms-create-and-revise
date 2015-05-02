1580b5531b6349180d1d3c82309916a8c6975f70
Anatoly Svichkarev <svichkarev.anatoly@gmail.com>
2015-05-02 14:40:23 +0600

    implement ListView example

--- app/src/main/java/com/example/anatoly/myapplication/MainActivity.java
+++ app/src/main/java/com/example/anatoly/myapplication/MainActivity.java
@@ -1,39 +1,25 @@
 package com.example.anatoly.myapplication;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+public class MainActivity extends Activity {
+    String[] values = {
+            "Android - an open-source operating system used for smartphones and tablet computers.",
+            "Google - search for information about (someone or something) on the Internet using the search engine Google."
+    };
 
-public class MainActivity extends ActionBarActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        ListView lvMain = (ListView) findViewById(R.id.lvMain);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, values);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        lvMain.setAdapter(adapter);
     }
 }
